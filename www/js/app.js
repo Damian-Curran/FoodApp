f979e8d24af23c9ddbe7cc6c444bbd51c8b6b94d
Damian <sgt.curren@gmail.com>
2016-08-17 01:30:24 +0100

    create food items

--- www/js/app.js
+++ www/js/app.js
@@ -2,4 +2,17 @@ angular.module('food', ['ionic'])
 
 .controller('foodCtrl', function($scope) {
   $scope.foods = [];
+  $scope.calorie = [];
+
+  
+  $scope.createFood = function(food) {
+    $scope.foods.push({
+      name: food.name,
+	  calorie: food.calorie
+    });
+	
+    food.name = "";
+	food.calorie = "";
+
+	};
 });
