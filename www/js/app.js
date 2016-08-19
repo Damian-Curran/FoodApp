848d5c693c2b60a9b06e90dd63a001b01f2584ba
Damian <sgt.curren@gmail.com>
2016-08-19 23:12:43 +0100

    added comments and list creation restriction

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,31 @@
+//<!-- sources used : https://www.youtube.com/watch?v=i9MHigUZKEM -->
+
 angular.module('food', ['ionic'])
 
 .controller('foodCtrl', function($scope) {
   $scope.foods = [];
   $scope.calorie = [];
 
-  
+  //function which sets entered food name and calorie value to the two arrays
   $scope.createFood = function(food) {
-    $scope.foods.push({
-      name: food.name,
-	  calorie: food.calorie
-    });
-	$scope.calorie.push({
-	  calorie: food.calorie
-    });
-	
-    food.name = "";
-	food.calorie = "";
+	  //if statement which doesnt allow creation of empty list item
+	  if(food.calorie >= 0)
+	  {
+		$scope.foods.push({
+		  name: food.name,
+		  calorie: food.calorie
+		});
+		$scope.calorie.push({
+		  calorie: food.calorie
+		});
+		//clears the input boxs
+		food.name = "";
+		food.calorie = "";
+	  }
 
 	};
 	
+	//function which reads through the calorie function and sums it and returns
 	$scope.totalCalorie = function () {
 	  var total = 0;
 	 
@@ -29,6 +36,8 @@ angular.module('food', ['ionic'])
 	 return total;
     };
 	
+	//function which takes values from item in list and creates new item in list with same values
+	//adds calorie value to calorie array to show total calories
 	$scope.duplicate = function (name, calorie) {
 		$scope.foods.push({
 		name: name,
